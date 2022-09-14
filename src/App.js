7b42fdf46a4c18e26c7d80464728768313e41e4a
jhconger <jhconger@gmail.com>
2022-09-14 00:31:55 -0500

    added more functionality of API results and styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import MovieBox from './MovieBox'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+const API_URL = "https://api.themoviedb.org/3/movie/popular?api_key=a79c10e281bddf600e942e8113e61d3a"
 function App() {
-  return (
-    <div>
-    Movie App
-    </div>
-  );
+
+    const [movies, setMovies] = useState([]);
+
+     useEffect(() => {
+        fetch(API_URL)
+            .then((res) => res.json())
+            .then(data => {
+                console.log(data);
+                setMovies(data.results);
+            })
+    }, []);
+
+    return (
+        <div className= "container">
+            <div className="grid">
+            {movies.map((movieReq) =>
+                <MovieBox key ={movieReq.id}{...movieReq}/>)}
+            </div>
+        </div>
+    );
 }
 
 export default App;
