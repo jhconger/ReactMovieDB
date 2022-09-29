9d6a7a9e8ff1d6983ef212911ac03a93939d6a99
jhconger <jhconger@gmail.com>
2022-09-28 22:46:07 -0500

    building for deployment

--- src/App.js
+++ src/App.js
@@ -59,57 +59,20 @@ function App() {
 
                 </Container>
             </Navbar>
-        <div className= "container">
-            <div className="grid">
-            {movies.map((movieReq) =>
-                <MovieBox key ={movieReq.id}{...movieReq}/>)}
+            <div>
+                {movies.length > 0 ?(
+                <div className= "container">
+                    <div className="grid">
+                    {movies.map((movieReq) =>
+                        <MovieBox key ={movieReq.id}{...movieReq}/>)}
+                    </div>
+                </div>
+                ):(
+                    <h2>Sorry! No moivies found.</h2>
+                )}
             </div>
-        </div>
         </>
     );
 }
-//         <>
-//             <Navbar bg="dark" expand="lg" variant="dark">
-//                 <Container fluid>
-//                     <Navbar.Brand href="/home">MovieDb App</Navbar.Brand>
-//                     <Navbar.Brand href="/home">Trending</Navbar.Brand>
-//                     <Navbar.Toggle aria-controls="navbarScroll"></Navbar.Toggle>
-//
-//                     <Navbar.Collapse id="nabarScroll">
-//                         <Nav
-//                             className="me-auto my-2 my-lg-3"
-//                             style={{maxHeight:'100px'}}
-//                             navbarScroll></Nav>
-//
-//                         <Form className="d-flex" onSubmit={searchMovie} autoComplete="off">
-//                             <FormControl
-//                                 type="search"
-//                                 placeholder="Movie Search"
-//                                 className="me-2"
-//                                 aria-label="search"
-//                                 name="query"
-//                                 value={query} onChange={changeHandler}></FormControl>
-//                             <Button variant="secondary" type="submit">Search</Button>
-//                         </Form>
-//                     </Navbar.Collapse>
-//                 </Container>
-//             </Navbar>
-//             <div>
-//                 {movies.length > 0 ?(
-//                     <div className="container">
-//                         <div className="grid">
-//                             {movies.map((movieReq)=>
-//                                 <MovieBox key={movieReq.id} {...movieReq}/>)}
-//                         </div>
-//                     </div>
-//                 ):(
-//                     <h2>Sorry !! No Movies Found</h2>
-//                 )}
-//             </div>
-//         </>
-//
-//     );
-// }
-
 
 export default App;
